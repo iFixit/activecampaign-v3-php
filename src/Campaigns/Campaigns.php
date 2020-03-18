b464136964478190776fe41e2e912ca62279f1af
David Rans <david@ifixit.com>
2020-03-17 17:03:00 -0700

    Campaigns: add new endpoints

---
+++ src/Campaigns/Campaigns.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Mediatoolkit\ActiveCampaign\Campaigns;
+
+use Mediatoolkit\ActiveCampaign\Resource;
+
+/**
+ * Class Campaigns
+ * @package Mediatoolkit\ActiveCampaign\Campaigns
+ * @see https://developers.activecampaign.com/reference#test-1
+ */
+class Campaigns extends Resource
+{
+
+    /**
+     * List all contacts
+     * @see https://developers.activecampaign.com/reference#list-all-campaigns
+     *
+     * @param array $query_params
+     * @param int $limit
+     * @param int $offset
+     * @return string
+     */
+    public function listAll(array $query_params = [], int $limit = 20, int $offset = 0)
+    {
+        $query_params = array_merge($query_params, [
+            'limit' => $limit,
+            'offset' => $offset
+        ]);
+
+        $req = $this->client
+            ->getClient()
+            ->get('/api/3/campaigns', [
+                'query' => $query_params
+            ]);
+
+        return $req->getBody()->getContents();
+    }
+
+    /**
+     * Get Campaign message
+     * @see https://developers.activecampaign.com/reference#retrieve-a-campaign
+     *
+     * @param int $campaignid
+     * @return string
+     */
+    public function getMessage(int $campaignid)
+    {
+        $req = $this->client
+            ->getClient()
+            ->get("/api/3/campaignMessages/{$campaignid}/message");
+
+        return $req->getBody()->getContents();
+    }
+}
